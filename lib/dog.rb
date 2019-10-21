521093a08c7bdf64877521e09e56e79c981d878e
Andrew Capp <acapp909@gmail.com>
2019-10-21 23:58:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+def Dog
+
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+
+  def name
+    @this_dog_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+  
+
+
+end
